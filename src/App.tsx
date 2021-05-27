5f5c01af3a24350a7e3d886399ab1527fd748b7b
rmunoz <richosojason@msn.com>
2021-05-26 22:38:53 -0500

    added list of posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import useData from "./hooks/useData";
 
-function App() {
+function App(): JSX.Element {
+  const posts = useData();
+
   return (
     <div className="App">
       <h1>React Guard Clauses</h1>
+      <ul>
+        {posts?.map((post) => (
+          <li key={post.id}>
+            {post.id} {post.title} {post.body}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
